dfa3f3163ba6f5dc01958912123e4e7d2317643e
chkramesh <chkramesh@gmail.com>
2019-05-02 12:04:19 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  form: FormGroup;
+
+  disableTextbox =  false;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      address: [{ value: 'a', disabled: true }],
+      street: [''],
+      zip: [''],
+    });
+  }
+
+  ngOnInit() {
+    this.form.patchValue({
+      name: 'name',
+      lastName: 'Last Name'
+    });
+
+    // this.form.disable();
+  }
+
+  update() {
+    console.log(this.form.value);
+
+    this.disableTextbox = !this.disableTextbox;
+    // this.disableTextbox = true;;
+  }
+
 }
